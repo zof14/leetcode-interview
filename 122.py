02ed9f21d5277f5a66e6b0f8c5ff94b70e0faa01
Zofia S <zosiasadel11@gmail.com>
2025-12-23 12:40:54 +0100

    task 122

---
+++ 122.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProfit(self, prices: List[int]):
+        total=0
+        
+        for i in range(len(prices)-1):
+            if prices[i+1]>prices[i]:
+                total+= prices[i+1]-prices[i]
+        
+        return total
+        
